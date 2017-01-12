c4d6cfca2b86da2d167516a90c7bc9790d6f2c56
Clementm916 <clementm916@gmail.com>
2017-01-12 15:53:22 +0300

    adding resources to api

--- app/api_v1/__init__.py
+++ app/api_v1/__init__.py
@@ -1,14 +1,17 @@
 from flask_restful import Api
 from app import app
-from .resources import RegisterApi,LoginApi,BucketlistsApi,BucketlistApi,BucketItemsApi
-from flasgger import Swagger
+from .resources import RegisterApi, LoginApi, BucketlistsApi, BucketlistApi, BucketItemsApi, GetTokenApi, BucketlistItemCreateApi
+
 
 api = Api(app=app, prefix='/api/v1')
 
 
-
 api.add_resource(RegisterApi, '/auth/register', endpoint='register')
 api.add_resource(LoginApi, '/auth/login', endpoint='login')
+api.add_resource(GetTokenApi, '/auth/token', endpoint='token')
 api.add_resource(BucketlistsApi, '/bucketlists/', endpoint='lists')
 api.add_resource(BucketlistApi, '/bucketlists/<int:id>', endpoint='list')
-api.add_resource(BucketItemsApi, '/bucketlists/<int:id>/items/<int:item_id>', endpoint='item')
+api.add_resource(
+    BucketlistItemCreateApi, '/bucketlists/<int:id>/items/', endpoint='item_create')
+api.add_resource(
+    BucketItemsApi, '/bucketlists/<int:id>/items/<int:item_id>', endpoint='item')
